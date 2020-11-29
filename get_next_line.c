9b0213a3ebedbfbfe1aa0daafa3b901a107ab095
c450coleoptere <nicolasrubin@protonmail.com>
2020-11-29 16:57:11 +0100

    updating work

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,23 @@
 /*   By: nrubin <nrubin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 12:38:53 by nrubin            #+#    #+#             */
-/*   Updated: 2020/11/27 19:09:55 by nrubin           ###   ########.fr       */
+/*   Updated: 2020/11/27 19:26:46 by nrubin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	get_next_line(int fd, char **line)
-{
-}
+#include <sys/stat.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 int	main(void)
 {
+	int fd;
+
+	fd = open("test.txt", O_RDONLY);
+
+	printf("%i\n", fd);
+
+	read(fd, )
+
 }
